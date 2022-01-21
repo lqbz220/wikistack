79f1e99816220932d7d9edf5557e947958c21465
Pamela Jung <pamjung@gmail.com>
2022-01-21 16:07:10 -0500

    add views and styling

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const path = require ('path');
+const path = require("path");
+const layout = require('./views/layout');
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res)=>{
-    res.send(console.log('hello world'))});
+app.get("/", (req, res) => {
+  res.send(layout('we are cool!'));
+});
 
-const PORT=1337;
-app.listen(PORT, () =>{console.log('Server is running!')});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server is running!");
+});
