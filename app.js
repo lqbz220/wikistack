50ccd7410c42794cc308051389cfa3477216938a
Pamela Jung <pamjung@gmail.com>
2022-01-21 16:20:38 -0500

    connected to db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const app = express();
 const path = require("path");
 const layout = require('./views/layout');
+const { db } = require('./models');
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,6 +12,15 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send(layout('we are cool!'));
 });
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
