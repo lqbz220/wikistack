3c639e294622233b37763c6fb0f857156184a3df
Lei Shi <leishi567@gmail.com>
2022-01-21 16:38:18 -0500

    added the tables

--- models/index.js
+++ models/index.js
@@ -1,14 +1,34 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
+const Page = db.define('page',{
+ title:{
+   type: Sequelize.STRING(100),
+   allowNull: false
+ },
+ slug:{
+  type: Sequelize.STRING(100),
+  allowNull: false
+ },
+ content:{
+  type: Sequelize.TEXT,
+  allowNull: false
+},
+status:{
+  type: Sequelize.ENUM ('open','closed'),
+  allowNull: false
+},
+})
 
-
-
-
-
-
-
+const User = db.define ('user',{
+  name:{
+    type: Sequelize.STRING(100),
+  },
+  email:{
+   type: Sequelize.STRING(100)
+  },
+})
 
 module.exports = {
-  db
+  db, Page, User
 }
